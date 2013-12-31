a7af87d9753f1dc1b6a23d260d6a2f5688cd1854
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-12-31 13:05:19 +1100

    Initial empty commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Basic GIS (geodjango) support for Microsoft SQL Server, built on top
+of [django-pyodbc](https://github.com/lionheart/django-pyodbc)
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="django-pyodbc-gis",
+    version="0.0.1",
+    author="Mark Hepburn",
+    author_email="mark@condense.com.au",
+    description=("GIS support for SQL Server, on top of django-pyodbc"),
+    license="BSD",
+    keywords="django mssql gis",
+    url="https://www.github.com/condense/django-pyodbc-gis",
+    packages=['django_pyodbc_gis'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Database",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        "django-pyodbc",
+    ],
+)
