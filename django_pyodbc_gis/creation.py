53dbe7f71f70c40ec156875c24633c62e169c489
Mark Hepburn <Mark.Hepburn@gmail.com>
2014-01-04 15:27:23 +1100

    Creation now also adds a geometry-type constraint

--- django_pyodbc_gis/creation.py
+++ django_pyodbc_gis/creation.py
@@ -9,24 +9,37 @@ class MSSqlCreation(DatabaseCreation):
     destruction of test databases.
     """
 
-    # SQL Server spatial index reference:
-    # http://technet.microsoft.com/en-us/library/bb934196.aspx
     def sql_indexes_for_field(self, model, f, style):
         from django.contrib.gis.db.models.fields import GeometryField
         output = super(MySQLCreation, self).sql_indexes_for_field(model, f, style)
 
-        if isinstance(f, GeometryField) and f.spatial_index:
+        if isinstance(f, GeometryField):
             qn = self.connection.ops.quote_name
             db_table = model._meta.db_table
-            idx_name = '%s_%s_id' % (db_table, f.column)
-            extent = str(f._extent)
-            output.append(style.SQL_KEYWORD('CREATE SPATIAL INDEX ') +
-                          style.SQL_TABLE(qn(idx_name)) +
-                          style.SQL_KEYWORD(' ON ') +
-                          style.SQL_TABLE(qn(db_table)) + '(' +
-                          style.SQL_FIELD(qn(f.column)) + ') ' +
-                          style.SQL_KEYWORD('USING GEOMETRY_AUTO_GRID ') +
-                          style.SQL_KEYWORD('WITH ') + '(' +
-                          style.SQL_KEYWORD('BOUNDING_BOX') + ' = ' +
-                          extent + ' );')
+
+            # Geometry-type constraint:
+            output.append(style.SQL_KEYWORD('ALTER TABLE ') +
+                          style.SQL_TABLE(qn(db_table)) +
+                          style.SQL_KEYWORD('ADD CONSTRAINT ') +
+                          style.SQL_TABLE(qn(constraint_name)) +
+                          style.SQL_KEYWORD(' CHECK ( ') +
+                          style.SQL_FIELD(qn(f.column)) +
+                          '.STGeometryType() = \'' +
+                          style.SQL_KEYWORD(f.geom_type) + '\' )')
+
+            # Spatial index; see:
+            # http://technet.microsoft.com/en-us/library/bb934196.aspx
+            if f.spatial_index:
+                idx_name = '%s_%s_id' % (db_table, f.column)
+                extent = str(f._extent)
+                output.append(style.SQL_KEYWORD('CREATE SPATIAL INDEX ') +
+                              style.SQL_TABLE(qn(idx_name)) +
+                              style.SQL_KEYWORD(' ON ') +
+                              style.SQL_TABLE(qn(db_table)) + '(' +
+                              style.SQL_FIELD(qn(f.column)) + ') ' +
+                              style.SQL_KEYWORD('USING GEOMETRY_AUTO_GRID ') +
+                              style.SQL_KEYWORD('WITH ') + '(' +
+                              style.SQL_KEYWORD('BOUNDING_BOX') + ' = ' +
+                              extent + ' )')
+
         return output
